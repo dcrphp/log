20eaa0247fa57296209f9554a6e1f38864fa46c7
huanghuanjun <junqing124@126.com>
2020-06-03 20:27:30 +0800

    删除测试文件

--- example/log/2020-06-02-17.php
+++
@@ -1,1 +0,0 @@
-[2020-06-02T17:28:02.012110+08:00] log.WARNING: Information message {"user":"AUSU"} []
--- example/log/2020-06-02.php
+++
@@ -1,2 +0,0 @@
-[2020-06-02T16:57:12.647189+08:00] log.WARNING: Information message {"user":"AUSU"} []
-[2020-06-02T17:27:51.414193+08:00] log.WARNING: Information message {"user":"AUSU"} []
